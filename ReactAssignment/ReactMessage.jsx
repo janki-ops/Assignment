e26966830c5fd8581f89e15a0d9112062abda395
janki-ops <mulanijanki051@gmail.com>
2025-12-03 18:56:53 +0530

    Add files via upload

---
+++ ReactAssignment/ReactMessage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ReactMessage extends Component {
+  render() {
+    return (
+        <div>
+      <h3>Welcome React!</h3>
+      </div>
+    );
+  }
+}
